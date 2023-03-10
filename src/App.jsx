49d8e6fb9ff0da7c72642d111dc15b81bb151558
GentileLuca <lugenba@gmail.com>
2023-03-10 13:33:35 +0100

    index.css eliminato

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './styles/App.css'
 
 
-import Button from './components/button'
+//import Button from './components/button'
 import EditorialContent from './components/EditorialContent'
 
 
