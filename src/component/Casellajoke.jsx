e7e8ec2b1a001c757478c3c39d657ccabb6ed937
GentileLuca <lugenba@gmail.com>
2023-03-14 18:38:31 +0100

    many fix
    fixati vari problemi di visualizzazione

--- src/component/Casellajoke.jsx
+++ src/component/Casellajoke.jsx
@@ -4,7 +4,12 @@ import '../styles/casellajoke.css'
 function Casellajoke(){
   return (
     <div className="casella">
-            <p>Chuck Norris can divide by zero. </p>
+            <p>
+              Chuck Norris can divide by zero.
+              Chuck Norris can divide by zero.
+              Chuck Norris can divide by zero.
+              Chuck Norris can divide by zero. 
+            </p>
     </div>
   )
 }
